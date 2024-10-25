fef29ead0b9af29725fd602b64a02d2409dfba6b
Leonis <119702124+the-gravity-destroyer@users.noreply.github.com>
2024-10-25 18:02:12 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hallo, was möchten Sie für ein Eis?");
+        System.out.println("vanille");
+        System.out.println("das macht dann 2 Euro!");
+    }
+
+}
